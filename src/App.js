8f56b8518829ef7938a7fd5f36f66dbcfa1f1e29
AjitkumarGovindaraj <ajitk5555@gmail.com>
2021-09-06 14:11:41 +0530

    delete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import {
-  Container,
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-} from 'reactstrap';
+import { Container } from 'reactstrap';
 import TableContainer from './TableContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SelectColumnFilter } from './filters';
@@ -23,31 +16,8 @@ const App = () => {
     };
     doFetch();
   }, []);
-
-  const renderRowSubComponent = (row) => {
-    const {
-      name: { first, last },
-      location: { city, street, postcode },
-      picture,
-      cell,
-    } = row.original;
-    return (
-      <Card style={{ width: '18rem', margin: '0 auto' }}>
-        <CardImg top src={picture.large} alt='Card image cap' />
-        <CardBody>
-          <CardTitle>
-            <strong>{`${first} ${last}`} </strong>
-          </CardTitle>
-          <CardText>
-            <strong>Phone</strong>: {cell} <br />
-            <strong>Address:</strong>{' '}
-            {`${street.name} ${street.number} - ${postcode} - ${city}`}
-          </CardText>
-        </CardBody>
-      </Card>
-    );
-  };
-
+  
+     
   const columns = useMemo(
     () => [
      
@@ -83,7 +53,7 @@ const App = () => {
       <TableContainer
         columns={columns}
         data={data}
-        renderRowSubComponent={renderRowSubComponent}
+      
       />
     </Container>
   );
